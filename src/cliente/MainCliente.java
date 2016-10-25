146364a353ed02fa74418a705016a27dd4d67832
William Leandro <royalty2012wla@gmail.com>
2016-10-25 11:12:10 -0300

    Se agregan cambios menores .

--- src/cliente/MainCliente.java
+++ src/cliente/MainCliente.java
@@ -7,24 +7,11 @@ import java.util.Scanner;
 public class MainCliente {
 
     public static void main(String args[]) throws IOException {
-
-        String host;
-        int puerto;
+    	Scanner scCliente;
 
         System.out.println("CLIENTE DE CHAT POR CONSOLA:\n");
 
-        System.out.println("Ingrese la IP o Nombre del Servidor a conectarse: ");
-
-        Scanner scCliente = new Scanner(new InputStreamReader(System.in));
-        host = scCliente.nextLine();
-        
-        System.out.println("Ingrese el puerto del servidor a conectar: ");
-
-        scCliente = new Scanner(new InputStreamReader(System.in));
-        puerto = scCliente.nextInt();
-       
-
-        Cliente cliente = new Cliente(host, puerto);
+        Cliente cliente = new Cliente();
         
         System.out.println("Ingrese el nombre de la sala: ");
         scCliente = new Scanner(new InputStreamReader(System.in));
