4e69ebbf4ec389ae34f1b3acccee3a0ce3a2cfaa
William Leandro <royalty2012wla@gmail.com>
2016-10-25 10:30:53 -0300

    Cambios menores con respecto a los cambios de Servidor.java .

--- src/servidor/MainServidor.java
+++ src/servidor/MainServidor.java
@@ -1,33 +1,18 @@
 package servidor;
 
 import java.net.Socket;
+
 public class MainServidor {
     public static void main(String[] args) {
         Socket socket = null;
 
         Servidor servidor = null;
-        int puerto = 3000;
         int maximoConexiones = 10;
 
-        System.out.println("SERVIDOR DE CHAT POR CONSOLA:\n");
-     
+        System.out.println("	SERVIDOR DE CHAT POR CONSOLA	\n");
+        servidor = new Servidor(maximoConexiones);
 
-        try {
-            puerto = 3000;
-        } catch (Exception e) {
-            System.out.println("Error al ingresar el puerto, cerrando aplicacion...");
-            System.exit(1);
-        }
-        try {
-            maximoConexiones = 10;
-        } catch (Exception e) {
-            System.out.println("Error al ingresar el puerto, cerrando aplicacion...");
-            System.exit(1);
-        }
-        servidor = new Servidor(puerto, maximoConexiones);
-
-        System.out.println("******	DATOS DEL SERVIDOR	******\n");
-        System.out.println("Nombre del Servidor:\t" + servidor.getNombreServidor());
+        System.out.println("********	DATOS DEL SERVIDOR	********\n");
         System.out.println("IP del Servidor:\t" + servidor.getIPServidor());
         System.out.println("Puerto de Escucha:\t" + servidor.getPuerto());
         System.out.println("\nServidor en espera...");
